b5034365eb78e1a79f3ddc9bb1e6badabf4415c0
moaaz.elayyouty <moaaz.elayyouty@dedalus.com>
2025-05-18 16:02:40 +0300

    modify testcases

--- src/test/java/tasks/TaskSession1.java
+++ src/test/java/tasks/TaskSession1.java
@@ -1,6 +1,5 @@
 package tasks;
 
-//import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
