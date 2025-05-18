f1301d62cf3c80314857e5ba3c94d3decdef98bf
moaaz.elayyouty <moaaz.elayyouty@dedalus.com>
2025-05-18 16:01:26 +0300

    modify testcases

--- src/test/java/tasks/TaskSession1.java
+++ src/test/java/tasks/TaskSession1.java
@@ -1,6 +1,7 @@
 package tasks;
 
 //import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,49 +12,42 @@ public class TaskSession1 {
 
     @Test
     public void validLogin() {
-//        WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.linkText("Form Authentication")).click();
-
-        String actualMessage = driver.findElement(By.xpath("//h2[contains(text(),'Login Page')]")).getText();
-        Assert.assertEquals(actualMessage, "Login Page");
-
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-
+        String actualMessage= driver.findElement(By.xpath("//h2[contains(text(),'Login Page')]")).getText();
+        String expectedMessage = "Login Page";
+        Assert.assertEquals(actualMessage,expectedMessage);
+        driver.findElement(By.xpath("//*[@id='username']")).sendKeys("tomsmith");
         String password = System.getenv("PASSWORD");
-        Assert.assertNotNull(password, "Environment variable 'PASSWORD' is not set");
-
-        driver.findElement(By.name("password")).sendKeys(password);
-        driver.findElement(By.cssSelector(".radius")).click();
-
+        driver.findElement(By.cssSelector("[name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//*[contains(@class, 'radius')]")).click();
         String successLoginMessage = driver.findElement(By.cssSelector(".flash.success")).getText();
-        Assert.assertTrue(successLoginMessage.contains("You logged into a secure area!"));
-
+        String expectedSuccessMessage= "You logged into a secure area!";
+        Assert.assertTrue(successLoginMessage.contains(expectedSuccessMessage));
         driver.quit();
+
     }
 
     @Test
     public void inValidLogin() {
-//        WebDriverManager.chromedriver().setup();  // ✅ Setup WebDriver
 
-        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.linkText("Form Authentication")).click();
-
-        String actualMessage = driver.findElement(By.xpath("//h2[contains(text(),'Login Page')]")).getText();
-        Assert.assertEquals(actualMessage, "Login Page");
-
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.name("password")).sendKeys("wrongpassword");
-        driver.findElement(By.cssSelector(".radius")).click();
-
-        String errorMessage = driver.findElement(By.id("flash")).getText();
-        Assert.assertTrue(errorMessage.contains("Your password is invalid!"));
-
+        String actualMessage= driver.findElement(By.xpath("//h2[contains(text(),'Login Page')]")).getText();
+        String expectedMessage = "Login Page";
+        Assert.assertEquals(actualMessage,expectedMessage);
+        driver.findElement(By.xpath("//*[@id='username']")).sendKeys("tomsmith");
+        driver.findElement(By.cssSelector("[name='password']")).sendKeys("password");
+        driver.findElement(By.xpath("//*[contains(@class, 'radius')]")).click();
+        String getActualErrorMessage = driver.findElement(By.id("flash")).getText();
+        Assert.assertTrue(getActualErrorMessage.contains("Your password is invalid!"));
         driver.quit();
     }
 }
