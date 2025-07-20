b0a29d2b4e95e92e8be30a075823a77e01c7b5a0
moaaz.elayyouty <moaaz.elayyouty@dedalus.com>
2025-07-20 10:05:53 +0300

    Github Actions

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -3,6 +3,7 @@ package Tests;
 import Pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -11,10 +12,18 @@ public class BaseTest {
     protected WebDriver driver;
     protected HomePage homePage;
 
-
     @BeforeClass
     public void setUpDriver() {
-        driver = new ChromeDriver();
+        // إعداد خيارات Headless Mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // تشغيل بدون واجهة رسومية
+        options.addArguments("--no-sandbox"); // لتفادي مشاكل CI
+        options.addArguments("--disable-dev-shm-usage"); // منع مشاكل الذاكرة المشتركة
+
+        // تشغيل Chrome بـ options
+        driver = new ChromeDriver(options);
+
+        // باقي الإعدادات
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
@@ -22,12 +31,8 @@ public class BaseTest {
 
     @AfterClass
     public void quitDriver() {
-
-//        if (driver != null) {
-//            driver.quit();
-//        }
-
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
-
-
